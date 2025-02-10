3050d0843bf768e0bdeaeec125ffd9c5dbfea66b
Sonu64 <sonusantu64@gmail.com>
2025-02-10 15:39:22 +0530

    Update Mover.cs

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 public class Mover : MonoBehaviour
 {
 
+    [SerializeField] float moveSpeed = 2.0f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start() 
     {
@@ -12,9 +14,9 @@ public class Mover : MonoBehaviour
     // Update is called once per frame
     void Update() 
     {
-        float moveX = Input.GetAxis("Horizontal"); // changed through controls
-        float moveY = 0.34f; // changed for github desktop demo
-        float moveZ = Input.GetAxis("Vertical");
+        float moveX = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime; // changed through controls
+        float moveY = 0f; // changed for github desktop demo
+        float moveZ = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
 
         transform.Translate(moveX, moveY, moveZ);
         //transform.Translate(-moveX, -moveY, -moveZ);

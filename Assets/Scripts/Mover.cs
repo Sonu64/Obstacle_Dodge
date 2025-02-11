c31982d307ff0bb5b687424b8d6a6a2e30eb7983
Sonu64 <sonusantu64@gmail.com>
2025-02-11 16:37:20 +0530

    Update Mover.cs

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -5,13 +5,25 @@ public class Mover : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start() 
     {
-        
+        PrintInsructions();
     }
 
     // Update is called once per frame
     void Update() 
     {
-        /*
+        MovePlayer();
+    }
+
+    void PrintInsructions()
+    {
+        Debug.Log("Welcome to Obstacle Dodger");
+        Debug.Log("Avoid obstacles to win the game !");
+        Debug.Log("Good Luck with your Ducky !");
+    }
+
+    void MovePlayer()
+    {
+         /*
          The Horizontal and Vertical issue continues don't know why, Faced this issue before.
          */
         float moveX = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime; 

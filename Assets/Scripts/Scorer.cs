5cfdd2bcb65a46806126a1345f426e4a0889d5b8
Sonu64 <sonusantu64@gmail.com>
2025-02-12 19:03:06 +0530

    Update Scorer.cs

--- Assets/Scripts/Scorer.cs
+++ Assets/Scripts/Scorer.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class Scorer : MonoBehaviour
 {
     int numberOfHits = 0;
+    bool isHitting  = false;
     private void OnCollisionEnter(Collision collision)
     {
         numberOfHits = numberOfHits + 1; // being called twice if Capsule collider is On on Ducky

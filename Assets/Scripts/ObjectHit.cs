2995043bc42e666272314f444d73c48af33fcbc6
Sonu64 <sonusantu64@gmail.com>
2025-02-12 10:27:09 +0530

    Added GetComponent to change color when hit

--- Assets/Scripts/ObjectHit.cs
+++ Assets/Scripts/ObjectHit.cs
@@ -5,6 +5,7 @@ public class ObjectHit : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void OnCollisionEnter(Collision collision)
     {
+        GetComponent<MeshRenderer>().material.color = Color.red;
         Debug.Log("Collided !");
     }
 }

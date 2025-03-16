54a5ada55b31d22419706768ff2ee39c3e243b55
Sonu64 <sonusantu64@gmail.com>
2025-03-16 12:45:46 +0530

    Added Awake() instead of Start() to activate projectiles
    BUG FIX:  Awake() is called whenever gameObject is activated for the 1st time. Not when game starts. If we use Start() here, and the player manages not to go through Trigger volume, then still Projectiles will fly to Starting Player Position and not the new Player Position.

--- Assets/Scripts/TriggerProjectile.cs
+++ Assets/Scripts/TriggerProjectile.cs
@@ -5,6 +5,16 @@ public class TriggerProjectile : MonoBehaviour
 
     [SerializeField] GameObject ProjectileSet;
 
+
+    // Awake() is called whenever gameObject is activated for the 1st time. Not 
+    // when game starts. If we use Start() here, and the player manages not to go through
+    // Trigger volume, then still Projectiles will fly to Starting Player Position and not
+    // new Player Position.
+    // UNITY ORDER OF EXECUTION -> Awake() -> OnEnable() -> Reset() -> Start() for a gameObject.
+    private void Awake() {
+        ProjectileSet.SetActive(false);
+    }
+
     private void OnTriggerEnter(Collider other) {
         if (other.gameObject.tag == "Player")
             ProjectileSet.SetActive(true);
